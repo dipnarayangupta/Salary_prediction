e9de7aed3f2685fcacc9bb2fbf4229f4c305630d
DIP NARAYAN GUPTA <35843268+dipnarayangupta@users.noreply.github.com>
2020-05-04 11:51:07 +0530

    comments added

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
+#imorting libraries
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
+#creating flask app
 app = Flask(__name__)
+
+#loading trained model pickel file
 model = pickle.load(open('model.pkl', 'rb'))
 
+#Home route
 @app.route('/')
 def home():
     return render_template('index.html')
 
+#Routes to new tab call predict
 @app.route('/predict',methods=['POST'])
+
+#prediction of result based on input from html body tag
 def predict():
     '''
     For rendering results on HTML GUI
@@ -22,6 +30,6 @@ def predict():
 
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
-
+#running flask app
 if __name__ == "__main__":
     app.run(debug=True)
